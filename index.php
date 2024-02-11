5b0b46b296e3b3ac4e50ab8708426305f4d21922
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-02-11 20:05:55 +0800

    add: associative arrays

--- index.php
+++ index.php
@@ -10,19 +10,38 @@
 			<h1>Recommended Books</h1>
 
 			<?php 
-				$books = ['Go Androids', 'The langoliers', 'Hail mary'];
+				// $books = ['Go Androids', 'The langoliers', 'Hail mary'];
+				
+				$books = [
+					[
+						"title" => "Legend of Drizzt",
+						"author" => "RA Salvadore",
+						"url" => 'http://example.com'
+					],
+					[
+						"title" => "Bones and Ashes",
+						"author" => "Chris Evans",
+						"url" => 'http://example.com'
+					],
+					[
+						"title" => "Forged in Fire",
+						"author" => "Matthew Snorke",
+						"url" => 'http://example.com'
+					],
+
+				]
+
 			?>
 
 			<ul>
-				<!-- <?php 
-					//foreach ($books as $book) {
-					//	echo "<li>" . $book . "</li>";
-				//	}
-				?> -->
+				<?php foreach ($books as $book) : ?>
+					<li>
+						
+						<a href="<?= $book["url"] ?>">
+							<?= $book["title"]?>
+						</a>
+					</li>
 
-				<?php 
-					foreach ($books as $book): ?>
-						<li><?= $book ?></li>
 				<?php endforeach; ?>
 				
 			</ul>
