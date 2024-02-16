36eef65b64bf3e154e1a3dd55c6842293ae8d98a
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-02-16 21:02:00 +0800

    add: dynamic banners

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,16 +1,12 @@
 <!DOCTYPE html>
-  <?php require('partials/head.php'); ?>
-  <?php require('partials/nav.php'); ?>
+<?php require('partials/head.php'); ?>
+<?php require('partials/nav.php'); ?>
+<?php require('partials/banner.php'); ?>
 
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <!-- Your content -->
-      Contact
-    </div>
-  </main>
-  <?php require('partials/footer.php'); ?>
+<main>
+  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <!-- Your content -->
+    Contact
+  </div>
+</main>
+<?php require('partials/footer.php'); ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,16 +1,12 @@
   <?php require('partials/head.php'); ?>
   <?php require('partials/nav.php'); ?>
+  <?php require('partials/banner.php'); ?>
 
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
-    </div>
-  </header>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <!-- Your content -->
       Home
     </div>
   </main>
-  
+
   <?php require('partials/footer.php'); ?>
