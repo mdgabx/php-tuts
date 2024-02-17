86379d639437cdb7f86e735ff1f65237c0c1474a
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-02-17 15:09:08 +0800

    add: router.php

--- index.php
+++ index.php
@@ -2,14 +2,4 @@
 
 require("functions.php");
 
-// dd($_SERVER);
-
-$uri = $_SERVER["REQUEST_URI"];
-
-if ($uri === '/') {
-    require("controllers/index.php");
-} else if ($uri === '/about') {
-    require("controllers/about.php");
-} else if ($uri === '/contact'){
-    require("controllers/contact.php");
-}
+require("router.php");
