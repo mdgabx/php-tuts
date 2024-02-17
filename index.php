9f9178ff242611279a8bb499152d60fef192c365
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-02-17 14:02:17 +0800

    add: global variables

--- index.php
+++ index.php
@@ -2,4 +2,23 @@
 
 $heading = "Home";
 
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+// dd($_SERVER);
+
+// if ($_SERVER["REQUEST_URI"] === '/') {
+//     echo "bg-gray-900 text-white";
+// } else {
+//     echo "text-gray-300";
+// }
+
+// echo $_SERVER["REQUEST_URI"] === "/" ? "bg-gray-900 text-white" : "text-gray-300";
+
 require("views/index.view.php");
