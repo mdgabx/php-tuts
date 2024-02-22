4da96c96ee427de756de2009d8927a2fc94064b3
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-02-22 20:26:57 +0800

    add: refactor

--- functions.php
+++ functions.php
@@ -9,7 +9,14 @@ function dd($value)
     die();
 }
 
-
-function urlIs($value) {
+function urlIs($value)
+{
     return $_SERVER["REQUEST_URI"] === $value;
+}
+
+function authorize($condition, $status = Response::FORBIDDEN)
+{
+    if (!$condition) {
+        abort($status);
+    }
 }
