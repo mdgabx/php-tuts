c84644b87ab4d134e09e72c2a9fd8cde3fd17255
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-02-25 16:43:49 +0800

    add namespaces

--- Core/router.php
+++ Core/router.php
@@ -1,6 +1,6 @@
 <?php
 
-$routes = require('routes.php');
+$routes = require base_path('routes.php');
 
 
 // if ($uri === '/') {
@@ -18,7 +18,7 @@ function abort($code = 404)
 {
     http_response_code($code);
 
-    require("views/{$code}.php");
+    require base_path("views/{$code}.php");
 
     die();
 }
@@ -27,7 +27,7 @@ function abort($code = 404)
 function uriToController($uri, $routes)
 {
     if (array_key_exists($uri, $routes)) {
-        require($routes[$uri]);
+        require base_path($routes[$uri]);
     } else {
         abort();
     }
