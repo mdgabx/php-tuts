ffdb575b6ef5bf4e617c8b35a6604099fb430369
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-02-25 16:14:48 +0800

    spl_autoload_register

---
+++ core/Database.php
@@ -0,0 +1,53 @@
+<?php
+
+class Database
+{
+    public $connection;
+    public $statement;
+
+    public function __construct($config, $username = "root", $password = "secret")
+    {
+        // dd("test");
+        $dsn = "mysql:" . http_build_query($config, "", ";");
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query, $params = [])
+    {
+        // Set PDO error mode to exceptions
+        //  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $this->statement = $this->connection->prepare($query);
+        $this->statement->execute($params);
+
+        // return $statement;
+        return $this;
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
+    }
+
+
+    public function findOrFail()
+    {
+        $result = $this->find();
+
+        if (!$result) {
+            abort();
+        }
+
+        return $result;
+    }
+
+    public function get()
+    {
+        return $this->statement->fetchAll();
+    }
+}
+
+
--- public/index.php
+++ public/index.php
@@ -5,8 +5,14 @@ const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . "functions.php";
 
 
-require base_path("Database.php");
-require base_path("Response.php");
+// require base_path("Database.php");
+// require base_path("Response.php");
+
+spl_autoload_register(function ($class) {
+    require base_path("core/{$class}.php");
+});
+
+
 require base_path("router.php");
 
 
