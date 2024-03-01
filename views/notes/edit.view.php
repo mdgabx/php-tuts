e2d154b9486e16d04cf2e63df4e94f9198f5e987
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-03-01 21:08:56 +0800

    add: edit validation

--- views/notes/edit.view.php
+++ views/notes/edit.view.php
@@ -6,7 +6,10 @@
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <div class="md:grid md:grid-cols-3 md:gap-6">
             <div class="mt-5 md:col-span-2 md:mt-0">
-                <form method="POST" action="/notes">
+                <form method="POST" action="/note">
+                    <input type="hidden" name="_method" value="PATCH" />
+                    <input type="hidden" name="id" value="<?= $note['id'] ?>" />
+
                     <div class="shadow sm:overflow-hidden sm:rounded-md">
                         <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
                             <div>
@@ -31,12 +34,18 @@
                             </div>
                         </div>
 
-                        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6 flex gap-x-4 justify-end">
+                            <a
+                                href="/notes"
+                                class="inline-flex justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
+                                Cancel
+                            </a>
                             <button
                                 type="submit"
                                 class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                             >
-                                Save
+                                Update
                             </button>
                         </div>
                     </div>
